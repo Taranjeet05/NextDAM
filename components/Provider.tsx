e602e94167cf40c34bf0500fe92a14ccb8e1cde1
taranjeet <taranjeetsinghtanu2@gmail.com>
2026-01-04 19:20:47 +0100

    chore: add global ImageKit and NextAuth providers

---
+++ components/Provider.tsx
@@ -0,0 +1,20 @@
+import { ImageKitProvider } from "@imagekit/next";
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
+
+type ProvidersProps = {
+  children: ReactNode;
+};
+
+const urlEndPoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
+
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <SessionProvider
+      refetchInterval={5 * 60} // 5 min
+      refetchOnWindowFocus={true}
+    >
+      <ImageKitProvider urlEndpoint={urlEndPoint}>{children}</ImageKitProvider>
+    </SessionProvider>
+  );
+}
